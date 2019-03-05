eb8e0bc530588b6bb87d0d8d0e6fc954818726d2
jhtong <me@joeltong.org>
2019-03-05 22:46:14 +0800

    reverting changes

--- src/index.js
+++ src/index.js
@@ -1,21 +1,8 @@
-import mongoose from 'mongoose';
-import { runUpdateUserService } from './lib/services/services';
-import Profile from './lib/models/Profile';
-
-const { MONGO_URI } = process.env;
-
-// connect to Mongo
-mongoose.connect(MONGO_URI);
-
-// load environment variables
-const dotenv = require('dotenv');
-dotenv.load();
 
 export default function app() {
-  console.log('Running Scraper..');
-  runUpdateUserService();
+    //begin writing your code here
 }
 
 if (require.main === module) {
-  app();
+    app();
 }
